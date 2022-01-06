6c2a7c4cbc34f88bfaa61f47a31554ff21c7f602
carl beattie <12beattiecastp@gmail.com>
2022-01-06 17:57:33 +0000

    change: changed the way postcodes were being created, now completely random

--- dataGenerators/userInformation/user.js
+++ dataGenerators/userInformation/user.js
@@ -1,5 +1,6 @@
 // This file will handle creating the users info like
 // FirstName, LastName, MiddleName, phone, email, address
+const { get } = require("http");
 const PATH = require("path");
 
 const __DATA_PATH = PATH.join(__dirname, "..\\..\\dataSets\\userInfo\\");
@@ -48,10 +49,44 @@ function personEmail(first_name, last_name) {
   return first_name + last_name + "@" + domains[globalFunctions.randomMinMax(0, domains.length)];
 }
 
-function postcode() {
-  let postcode = globalFunctions.getJSONContentArray("postcodes.json", __DATA_PATH);
+// valid postcode formats
+// AA9A AA
+// A9A 9AA
+// A9 9AA
+// A99 9AA
+// AA9 9AA
+// AA99 9AA
 
-  return postcode[globalFunctions.randomMinMax(0, postcode.length - 1)];
+function postcode() {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+  let maximumPatterns = 6;
+
+  const getChar = () => alphabet[globalFunctions.randomMinMax(0, alphabet.length - 1)];
+  const getNum = () => globalFunctions.randomMinMax(1, 9);
+
+  const pattenOne = () => getChar() + getChar() + getNum() + getChar() + " " + getChar() + getChar();
+  const pattenTwo = () => getChar() + getNum() + getChar() + " " + getNum() + getChar() + getChar();
+  const pattenThree = () => getChar() + getNum() + " " + getNum() + getChar() + getChar();
+  const pattenFour = () => getChar() + getNum() + getNum() + " " + getNum() + getChar() + getChar();
+  const pattenFive = () => getChar() + getChar() + getNum() + " " + getNum() + getChar() + getChar();
+  const pattenSix = () => getChar() + getChar() + getNum() + getNum() + " " + getNum() + getChar() + getChar();
+
+  switch (globalFunctions.randomMinMax(1, maximumPatterns)) {
+    case 1:
+      return pattenOne();
+    case 2:
+      return pattenTwo();
+    case 3:
+      return pattenThree();
+    case 4:
+      return pattenFour();
+    case 5:
+      return pattenFive();
+    case 6:
+      return pattenSix();
+    default:
+      console.error("Error: Out of range <postcode-max=6>");
+  }
 }
 
 function streetName() {
