440997b3c100d0be9cca0e2ed63a38caa984e20e
carl beattie <12beattiecastp@gmail.com>
2022-01-06 07:43:31 +0000

    removed: postcode.json

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const express = require("express");
-const app = require("express")();
-
 const dataGenerators = require("./dataGenerators");
 
-app.get("/", (req, res) => {
-  res.json(
-    dataGenerators.main.createMultipleUsers(
-      1,
-      "address",
-      "privateInfo=2",
-      "bankAccount",
-      "addTransaction=2",
-      "addFlag=1",
-      "loan"
-    )
-  );
-});
-
-app.listen(3000, () => {
-  console.log("web server open");
-});
-
-function sortStringToArray(string) {
-  return string.split(" ");
-}
+module.exports = dataGenerators;
