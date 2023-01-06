58fa5c12e24728f81e1edf4d067e0fb941be0446
carl beattie <12beattiecastp@gmail.com>
2023-01-06 03:01:22 +0000

    add: internet tests

---
+++ tests/modules/internet.test.js
@@ -0,0 +1,77 @@
+const { equal, match } = require("assert");
+const test = require("node:test");
+
+const impostor = require("../../src/impostor");
+
+const impostorInstance = new impostor({ countryCode: "en_GB" });
+
+test("internet tests", async (t) => {
+  const internetModule = impostorInstance.internet;
+
+  await t.test("IPV4 should return a string", () => {
+    const IPV4 = internetModule.IPV4();
+
+    equal(typeof(IPV4), typeof(""));
+  })
+
+  await t.test("IPV6 should return a string", () => {
+    const IPV6 = internetModule.IPV6();
+
+    equal(typeof(IPV6), typeof(""));
+  })
+
+  await t.test("IPV6 length should be 38", () => {
+    const IPV6 = internetModule.IPV6();
+
+    equal(IPV6.length, 36);
+  })
+
+  await t.test("domain should return a string", () => {
+    const domain = internetModule.domain();
+
+    equal(typeof(domain), "");
+  })
+
+  await t.test("domain should include a valid domain extension", () => {
+    const domain = internetModule.domain();
+
+    match(domain, /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/);
+  })
+
+  await t.test("URL should return a string", () => {
+    const URL = internetModule.URL();
+
+    equal(typeof(URL), typeof(""));
+  })
+
+  await t.test("URL should be a valid HTTPS url", () => {
+    const URL = internetModule.URL();
+    
+    match(URL, /^https:\/\/[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/);
+  })
+
+  await t.test("userAgent should return a string", () => {
+    const userAgent = internetModule.userAgent();
+
+    equal(typeof(userAgent), typeof(""));
+  })
+
+  await t.test("httpMethod should return a string", () => {
+    const httpMethod = internetModule.httpMethod();
+
+    equal(typeof(httpMethod), typeof(""));
+  })
+
+  await t.test("port should return a int", () => {
+    const port = internetModule.port();
+
+    equal(typeof(port), typeof(0));
+  })
+
+  await t.test("port shouldn't be less than 0 or greater than 49151", () => {
+    const port = internetModule.port();
+
+    equal(port < 0, false);
+    equal(port > 49151, false)
+  })
+})
