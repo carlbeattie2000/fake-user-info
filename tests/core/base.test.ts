6ac87d0be6b45ac1f4d19b6bbf19f48cb725a6ab
carl beattie <12beattiecastp@gmail.com>
2024-01-02 05:29:02 +0000

    add: last tests for base class

--- tests/core/base.test.ts
+++ tests/core/base.test.ts
@@ -85,3 +85,53 @@ test("generates a random date string", () => {
   expectTypeOf(dateStringSameAndCurrentYear).toBeString();
   expect(dateStringSameAndCurrentYear).toContain("2024");
 });
+
+test("selects a random array element", () => {
+  const charArray = ["a", "b", "c", "d", "e"];
+  const numberArray = [4, 44, 25, 1, 99, 101];
+  const emptyArray: number[] = [];
+
+  expect(() => testBase.randomArrayElement<number>(emptyArray)).toThrowError();
+
+  const randomChar = testBase.randomArrayElement<string>(charArray);
+  expectTypeOf(randomChar).toBeString();
+  expect(randomChar).toHaveLength(1);
+
+  const randomNumber = testBase.randomArrayElement<number>(numberArray);
+  expectTypeOf(randomNumber).toBeNumber();
+  expect(randomNumber).toBeGreaterThanOrEqual(4);
+  expect(randomNumber).toBeLessThanOrEqual(101);
+});
+
+test("generates random bytes of size", () => {
+  const randomBytes = testBase.randomBytes(24);
+  expect(randomBytes).toHaveLength(24);
+});
+
+test("generates random bytes string of size", () => {
+  const randomBytesString = testBase.randomBytesString(24);
+
+  expectTypeOf(randomBytesString).toBeString();
+  expect(randomBytesString).toHaveLength(24 * 2);
+});
+
+test("generates random string of size", () => {
+  const randomString = testBase.randomString(54);
+
+  expectTypeOf(randomString).toBeString();
+  expect(randomString).toHaveLength(54);
+});
+
+test("generates a random string using a custom template", () => {
+  const randomNumberTemplate = testBase.randomStringFormatter("###");
+  const randomYearTemplate = testBase.randomStringFormatter("YYYY");
+
+  expectTypeOf(randomNumberTemplate).toBeString();
+  expectTypeOf(randomYearTemplate).toBeString();
+
+  expect(randomNumberTemplate).toHaveLength(3);
+  expect(randomYearTemplate).toHaveLength(4);
+
+  expect(randomNumberTemplate).to.not.contain("#");
+  expect(randomYearTemplate).to.not.contain("Y");
+});
