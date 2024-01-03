b54d7a0d91e9543e07c4b5b6bb3e6bc75bb13631
carl beattie <12beattiecastp@gmail.com>
2024-01-03 19:43:29 +0000

    add: locale internet

--- src/locales/internet.ts
+++ src/locales/internet.ts
@@ -1,20 +1,58 @@
 import { Base } from "../core/base";
+import { UserAgent } from "../core/userAgents";
 import { Internet } from "../types/locales";
+import internetGlobalData from "../../src/data/internet.json";
 
 export class InternetLocale extends Base {
   internetData: Internet;
+  userAgentMethod: UserAgent;
 
   constructor(internetData: Internet) {
     super();
 
+    this.userAgentMethod = new UserAgent();
     this.internetData = internetData;
   }
 
   IPV4(): string {
-    return new Array(4).map((_) => this.randomInt({ max: 255 })).join(".");
+    return new Array(4)
+      .fill(0)
+      .map((_) => this.randomInt({ max: 255 }))
+      .join(".");
   }
 
   IPV6(): string {
-    return new Array(8).map((_) => this.randomBytesString(2)).join(":");
+    return new Array(8)
+      .fill(0)
+      .map((_) => this.randomBytesString(2))
+      .join(":");
+  }
+
+  domain(): string {
+    return (
+      this.randomArrayElement<string>(this.internetData.domainNames) +
+      "." +
+      this.randomArrayElement<string>(this.internetData.domains)
+    );
+  }
+
+  url(): string {
+    return `https://www.${this.domain()}`;
+  }
+
+  userAgent(): string {
+    return this.userAgentMethod.generate();
+  }
+
+  http(): string {
+    return this.randomArrayElement<string>(internetGlobalData.httpMethods);
+  }
+
+  port(): number {
+    return this.randomInt({ max: 49151 });
+  }
+
+  isp(): string {
+    return this.randomArrayElement<string>(this.internetData.isps);
   }
 }
