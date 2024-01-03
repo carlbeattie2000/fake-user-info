02c76f3ba95c167e79f6dabe8bd50270beaa5eba
carl beattie <12beattiecastp@gmail.com>
2024-01-03 01:12:00 +0000

    add: en_GB internet data

---
+++ tests/locales/internet.test.ts
@@ -0,0 +1,3 @@
+import { assertType, expectTypeOf, expect, test } from "vitest";
+import { InternetLocale } from "../../src/locales/internet";
+import enGBData from "../../src/data/locales/en_GB.json";
